ce3efe00cf812d89677434ab33a40086b80754d3
Tobi <tohbyyy@gmail.com>
2022-11-22 13:05:37 +0700

    Fix radio button

--- resources/views/admin/classes/takeAttendance.blade.php
+++ resources/views/admin/classes/takeAttendance.blade.php
@@ -1,6 +1,6 @@
 <div class="modal fade show" id="modal-attendance" tabindex="-1" role="dialog" aria-labelledby="modal-attendance"
     aria-modal="true">
-    <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h2 class="h6 modal-title">Take Attendance</h2><button type="button" class="btn-close"
@@ -11,19 +11,20 @@
                 <div class="modal-body">
                     @foreach ($class->students as $student)
                         <div class="row px-4">
-                            <div class="col-md-10">{{ $student->name }}
-                                <input type="hidden" name="student[]" value="{{ $student->id }}">
+                            <div class="col-md-8">{{ $student->name }}
+                                <input type="hidden" name="student[] {{ $student->id }}" value="{{ $student->id }}">
                             </div>
-                            <div class="col-md-1">
+                            <div class="col-md-2">
                                 <div class="form-check form-check-inline">
-                                    <input type="radio" class="form-check-input" name="status[]" value="1"
-                                        checked>Present
+                                    <input type="radio" class="form-check-input" id="{{ $student->id }}"
+                                        name="status[] {{ $student->id }}" value="1" checked>Present
                                     <label class="form-check-label" for="status"></label>
                                 </div>
                             </div>
-                            <div class="col-md-1">
+                            <div class="col-md-2">
                                 <div class="form-check form-check-inline">
-                                    <input type="radio" class="form-check-input" name="status[]" value="0">Absent
+                                    <input type="radio" class="form-check-input" id="{{ $student->id }}"
+                                        name="status[] {{ $student->id }}" value="0">Absent
                                     <label class="form-check-label" for="status"></label>
                                 </div>
                             </div>
